bc5c96cc90d62fe873f67947ca32701785f6620a
彭强 <pq1130@qq.com>
2020-01-02 21:34:02 +0800

    初始化

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index()
+        {
+            return "This is Blog Ttest Index Action";
+        }
+
+        public string Hello(string name, int id)
+        {
+            return $"This is Blog Hello Function Name {name } ID {id}";
+        }
+
+    }
+}
