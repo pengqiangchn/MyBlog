0d91e35eb1a6a6a158ff3e0ddc228da03d5f9051
彭强 <pq1130@qq.com>
2020-02-01 21:54:30 +0800

    修改一些错误的上传文件

---
+++ Infrastructure.Crosscutting.NetFramework/Adapter/AutomapperTypeAdapter.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Infrastructure.Crosscutting.Adapter;
+
+namespace Infrastructure.Crosscutting.NetFramework.Adapter
+{
+
+    /// <summary>
+    /// Automapper type adapter implementation
+    /// </summary>
+    public class AutomapperTypeAdapter
+        : ITypeAdapter
+    {
+        private readonly IMapper _mapper;
+
+        public AutomapperTypeAdapter(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        #region ITypeAdapter Members
+
+        public TTarget Adapt<TSource, TTarget>(TSource source)
+            where TSource : class
+            where TTarget : class, new()
+        {
+            return _mapper.Map<TSource, TTarget>(source);
+        }
+
+        public TTarget Adapt<TTarget>(object source) where TTarget : class, new()
+        {
+            return _mapper.Map<TTarget>(source);
+        }
+
+        #endregion
+    }
+}
---
+++ Infrastructure.Crosscutting.NetFramework/Localization/ResourcesManagerFactory.cs
@@ -0,0 +1,13 @@
+﻿
+using Infrastructure.Crosscutting.Localization;
+
+namespace Infrastructure.Crosscutting.NetFramework.Localization
+{
+    public class ResourcesManagerFactory : ILocalizationFactory
+    {
+        public ILocalization Create()
+        {
+            return new ResourcesManager();
+        }
+    }
+}
