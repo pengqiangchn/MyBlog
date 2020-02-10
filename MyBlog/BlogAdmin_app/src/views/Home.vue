1b2c31d5552f228ab2b6f1d6304fe1eee47d888e
彭强 <pq1130@qq.com>
2020-02-10 16:07:46 +0800

    添加 admin 使用Vue

---
+++ MyBlog/BlogAdmin_app/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <div class="home">这个是首页</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
+
+<style scoped>
+.home {
+  font-size: 20px;
+}
+</style>
